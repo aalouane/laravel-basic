f3e5e7b6c4ae3ac0da74ae7fb70ffe94989e6ffb
unknown <aalouane.s@gmail.com>
2022-12-29 21:27:32 +0100

    tests 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Demo\DemoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(DemoController::class)->groupe(function(){
+    Route::get('/about', 'about')->name('about.page');
+    Route::get('/contact', 'contact')->name('contact.page');
 });

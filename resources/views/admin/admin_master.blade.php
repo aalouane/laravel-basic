fdd0b960fc09e0492c6762e8c4bd7d24eec45c76
unknown <aalouane.s@gmail.com>
2022-12-30 16:25:13 +0100

    seperate content 2

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -24,12 +24,13 @@
         rel="stylesheet" type="text/css" />
 
     <!-- Bootstrap Css -->
-    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet"
-        type="text/css" />
+    {{-- <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style"--> rel="stylesheet" type="text/css" /> --}}
+    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
     <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+    {{-- <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" /> --}}
+    <link href="{{asset('backend/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
 
 </head>
 

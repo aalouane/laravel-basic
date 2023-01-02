a65bd0762d85437f3e9de6fa34b4b3e5b0f2548c
unknown <aalouane.s@gmail.com>
2023-01-02 15:13:25 +0100

    integrit login 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,8 +31,10 @@
                     <div class="text-center mt-4">
                         <div class="mb-3">
                             <a href="index.html" class="auth-logo">
-                                <img src="{{asset('backend/assets/images/logo-dark.png')}}" height="30" class="logo-dark mx-auto" alt="">
-                                <img src="{{asset('backend/assets/images/logo-light.png')}}" height="30" class="logo-light mx-auto" alt="">
+                                <img src="{{asset('backend/assets/images/logo-dark.png')}}" height="30"
+                                    class="logo-dark mx-auto" alt="">
+                                <img src="{{asset('backend/assets/images/logo-light.png')}}" height="30"
+                                    class="logo-light mx-auto" alt="">
                             </a>
                         </div>
                     </div>
@@ -40,25 +42,36 @@
                     <h4 class="text-muted text-center font-size-18"><b>Sign In</b></h4>
 
                     <div class="p-3">
-                        <form class="form-horizontal mt-3" action="index.html">
 
+                        <form class="form-horizontal mt-3" method="POST" action="{{ route('login') }}">
+
+                            @csrf
+
+                            <!-- Username -->
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" type="text" required="" placeholder="Username">
+                                    <input class="form-control" id="username" type="text" name="username"
+                                        :value="old('username')" required autofocus />
+                                    <x-input-error :messages="$errors->get('username')" class="mt-2" />
                                 </div>
                             </div>
 
+                            <!-- Password -->
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
-                                    <input class="form-control" type="password" required="" placeholder="Password">
+                                    <input class="form-control" id="password" type="password" name="password" required
+                                        autocomplete="current-password" />
+
+                                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
                                 </div>
                             </div>
 
+                            <!-- Remember Me -->
                             <div class="form-group mb-3 row">
                                 <div class="col-12">
                                     <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                        <label class="form-label ms-1" for="customCheck1">Remember me</label>
+                                        <input type="checkbox" class="custom-control-input" id="remember_me">
+                                        <label class="form-label ms-1" for="remember_me">{{ __('Remember me') }}</label>
                                     </div>
                                 </div>
                             </div>
@@ -72,11 +85,11 @@
 
                             <div class="form-group mb-0 row mt-2">
                                 <div class="col-sm-7 mt-3">
-                                    <a href="auth-recoverpw.html" class="text-muted"><i class="mdi mdi-lock"></i> Forgot
+                                    <a href="href="{{ route('password.request') }}" class="text-muted"><i class="mdi mdi-lock"></i> Forgot
                                         your password?</a>
                                 </div>
                                 <div class="col-sm-5 mt-3">
-                                    <a href="auth-register.html" class="text-muted"><i
+                                    <a href="{{route('register')}}" class="text-muted"><i
                                             class="mdi mdi-account-circle"></i> Create an account</a>
                                 </div>
                             </div>

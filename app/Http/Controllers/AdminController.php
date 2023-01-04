1cf07b087459c8a8ddbc273db761176d224fd843
unknown <aalouane.s@gmail.com>
2023-01-04 21:00:50 +0100

    workin on toast messages 2

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -71,10 +71,15 @@ class AdminController extends Controller
 
         $user->save();
 
-        return redirect()->route('admin.profile');
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('admin.profile')->with($notification);
     }
 
-    
+
     
 }
 

f6e53e2ef4817a1d1ab49c29ff9598b573542a68
unknown <aalouane.s@gmail.com>
2023-01-12 09:44:45 +0100

    about all multi image 3

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -142,10 +142,10 @@
 
     {{-- TODO:  Soufiane ! we can use if condition to costomize the loading file depending the page route --}}
     <!-- Required datatable js -->
-    <script src="assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-    <script src="assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
     <!-- Datatable init js -->
-    <script src="assets/js/pages/datatables.init.js"></script>
+    <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
 </body>
 
 </html>

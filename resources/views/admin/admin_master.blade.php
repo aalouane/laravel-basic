c23757abd5c6803a9247e6ac3b4c10071805b749
Aalouane soufiane <aalouane.s@gmail.com>
2023-02-02 12:09:59 +0100

    blog add 2

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -3,119 +3,121 @@
 
 <head>
 
-    <meta charset="utf-8" />
-    <title>Dashboard | Upcube - Admin & Dashboard Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-    <meta content="Themesdesign" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
+  <meta charset="utf-8" />
+  <title>Dashboard | Upcube - Admin & Dashboard Template</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+  <meta content="Themesdesign" name="author" />
+  <!-- App favicon -->
+  <link rel="shortcut icon" href="{{asset('backend/assets/images/favicon.ico')}}">
 
-    <!-- jquery.vectormap css -->
-    <link href="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}"
-        rel="stylesheet" type="text/css" />
+  <!-- jquery.vectormap css -->
+  <link href="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css')}}"
+    rel="stylesheet" type="text/css" />
 
-    <!-- DataTables -->
-    <link href="{{asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet"
-        type="text/css" />
+  <!-- DataTables -->
+  <link href="{{asset('backend/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet"
+    type="text/css" />
 
-    <!-- Responsive datatable examples -->
-    <link href="{{asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}"
-        rel="stylesheet" type="text/css" />
+  <!-- Responsive datatable examples -->
+  <link href="{{asset('backend/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}"
+    rel="stylesheet" type="text/css" />
 
-    <!-- Bootstrap Css -->
-    {{--
-    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" --> rel="stylesheet"
-    type="text/css" /> --}}
-    <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
-    <!-- Icons Css -->
-    <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-    <!-- App Css-->
-    {{--
-    <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" /> --}}
-    <link href="{{asset('backend/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+  <!-- Bootstrap Css -->
+  {{--
+  <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" --> rel="stylesheet"
+  type="text/css" /> --}}
+  <link href="{{asset('backend/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+  <!-- Icons Css -->
+  <link href="{{asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+  <!-- App Css-->
+  {{--
+  <link href="{{asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" /> --}}
+  <link href="{{asset('backend/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
 
-    <!-- Toast css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
-        integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- Toast css -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css"
+    integrity="sha512-vKMx8UnXk60zUwyUnUPM3HbQo8QfmNx7+ltw8Pm5zLusl1XIfwcxo8DbWCqMGKaWeNxWA8yrx5v3SaVpMvR3CA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+  {{-- Tagsinput css --}}
+  <link rel="stylesheet" type="text/css"
+    href="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.css">
 </head>
 
 <body data-topbar="dark">
 
-    <!-- <body data-layout="horizontal" data-topbar="dark"> -->
+  <!-- <body data-layout="horizontal" data-topbar="dark"> -->
 
-    <!-- Begin page -->
-    <div id="layout-wrapper">
-
-
-        @include('admin.body.header')
-
-        <!-- ========== Left Sidebar Start ========== -->
-        @include('admin.body.sidebar')
-        <!-- Left Sidebar End -->
+  <!-- Begin page -->
+  <div id="layout-wrapper">
 
 
+    @include('admin.body.header')
 
-        <!-- ============================================================== -->
-        <!-- Start right Content here -->
-        <!-- ============================================================== -->
-        <div class="main-content">
+    <!-- ========== Left Sidebar Start ========== -->
+    @include('admin.body.sidebar')
+    <!-- Left Sidebar End -->
 
-            @yield('admin')
-            <!-- End Page-content -->
 
-            @include('admin.body.footer')
 
-        </div>
-        <!-- end main content-->
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+      @yield('admin')
+      <!-- End Page-content -->
+
+      @include('admin.body.footer')
 
     </div>
-    <!-- END layout-wrapper -->
+    <!-- end main content-->
 
-    <!-- Right bar overlay-->
-    <div class="rightbar-overlay"></div>
+  </div>
+  <!-- END layout-wrapper -->
 
-    <!-- JAVASCRIPT -->
-    <script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+  <!-- Right bar overlay-->
+  <div class="rightbar-overlay"></div>
+
+  <!-- JAVASCRIPT -->
+  <script src="{{asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
 
 
-    <!-- apexcharts -->
-    <script src="{{asset('backend/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
+  <!-- apexcharts -->
+  <script src="{{asset('backend/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
 
-    <!-- jquery.vectormap map -->
-    <script src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}">
-    </script>
-    <script
-        src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js')}}">
-    </script>
+  <!-- jquery.vectormap map -->
+  <script src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js')}}">
+  </script>
+  <script src="{{asset('backend/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js')}}">
+  </script>
 
-    <!-- Required datatable js -->
-    <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+  <!-- Required datatable js -->
+  <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 
-    <!-- Responsive examples -->
-    <script src="{{asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}">
-    </script>
+  <!-- Responsive examples -->
+  <script src="{{asset('backend/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}">
+  </script>
 
-    <script src="{{asset('backend/assets/js/pages/dashboard.init.js')}}"></script>
+  <script src="{{asset('backend/assets/js/pages/dashboard.init.js')}}"></script>
 
-    <!-- App js -->
-    <script src="{{asset('backend/assets/js/app.js')}}"></script>
+  <!-- App js -->
+  <script src="{{asset('backend/assets/js/app.js')}}"></script>
 
-    <!-- Toast js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"
-        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <!-- Toast js -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"
+    integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script>
-        @if(Session::has('message'))
+  <script>
+    @if(Session::has('message'))
             var type = "{{ Session::get('alert-type','info') }}"
             switch(type){
                 case 'info':
@@ -132,24 +134,26 @@
                 break; 
             }
         @endif 
-    </script>
+  </script>
 
-    <!--tinymce js-->
-    <script src="{{asset('backend/assets/libs/tinymce/tinymce.min.js')}}"></script>
+  <!--tinymce js-->
+  <script src="{{asset('backend/assets/libs/tinymce/tinymce.min.js')}}"></script>
 
-    <!-- init js -->
-    <script src="{{asset('backend/assets/js/pages/form-editor.init.js')}}"></script>
+  <!-- init js -->
+  <script src="{{asset('backend/assets/js/pages/form-editor.init.js')}}"></script>
 
-    {{-- TODO:  Soufiane ! we can use if condition to costomize the loading file depending the page route --}}
-    <!-- Required datatable js -->
-    <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
-    <!-- Datatable init js -->
-    <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
+  {{-- TODO: Soufiane ! we can use if condition to costomize the loading file depending the page route --}}
+  <!-- Required datatable js -->
+  <script src="{{asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+  <!-- Datatable init js -->
+  <script src="{{asset('backend/assets/js/pages/datatables.init.js')}}"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    
-    <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+  <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+
+  <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
 </body>
 
 </html>
--- resources/views/admin/blog/add_blog.blade.php
+++ resources/views/admin/blog/add_blog.blade.php
@@ -4,6 +4,14 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
 
+<style type="text/css">
+  .bootstrap-tagsinput .tag {
+    margin-right: 2px;
+    color: #b70000;
+    font-weight: 700px;
+  }
+</style>
+
 <div class="page-content">
   <div class="container-fluid">
 
@@ -15,20 +23,64 @@
           <div class="card-body">
 
             <h4 class="card-title">Add Blog Page</h4>
-            <form action="{{ route('store.blog')}}" method="POST">
+            <form action="{{ route('store.portfolio')}}" method="POST" enctype="multipart/form-data">
               @csrf
               {{-- Edit Name --}}
               <div class="row mb-3">
-                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <label for="blog_category" class="col-sm-2 col-form-label">Blog Category</label>
                 <div class="col-sm-10">
-                  <input class="form-control" type="text" value="" id="name" name="name">
-                  @error('name')
+                  <select class="form-select" aria-label="Default select example" name="blog_category">
+                    <option selected="">Open this select menu</option>
+                    <option value="1">One</option>
+                  </select>
+                </div>
+              </div>
+
+              {{-- Edit title --}}
+              <div class="row mb-3">
+                <label for="title" class="col-sm-2 col-form-label">Title</label>
+                <div class="col-sm-10">
+                  <input class="form-control" type="text" value="" id="title" name="title">
+                  @error('title')
                   <span class="text-danger">{{ $message }}</span>
                   @enderror
                 </div>
               </div>
 
-              <input type="submit" class="btn btn-info waves-effect waves-light" value="Insert Blog Cateogry">
+              {{-- Edit tags --}}
+              <div class="row mb-3">
+                <label for="tags" class="col-sm-2 col-form-label">Tags</label>
+                <div class="col-sm-10">
+                  <input class="form-control" type="text" value="php,javascript" name="blog_tags" data-role="tagsinput">
+                </div>
+              </div>
+
+              {{-- Edit Long Description --}}
+              <div class="row mb-3">
+                <label for="elem1" class="col-sm-2 col-form-label">Description</label>
+                <div class="col-sm-10">
+                  <textarea id="elm1" name="description"></textarea>
+                </div>
+              </div>
+
+              {{-- Edit About Page Image --}}
+              <div class="row mb-3">
+                <label for="image" class="col-sm-2 col-form-label">Image</label>
+                <div class="col-sm-10">
+                  <input class="form-control" type="file" id="image" name="image">
+                </div>
+              </div>
+
+              {{-- Show Home Slide --}}
+              <div class="row mb-3">
+                <label for="" class="col-sm-2 col-form-label"></label>
+                <div class="col-sm-10">
+                  <img class="rounded avatar-lg " id="show_image" src="{{ asset('upload/no_image.jpg')}}"
+                    alt="Card image cap">
+                </div>
+              </div>
+
+              <input type="submit" class="btn btn-info waves-effect waves-light" value="Insert Portfolio Page">
             </form>
           </div>
 
@@ -38,5 +90,19 @@
 
   </div>
 </div>
+
+
+<script type="text/javascript">
+  $(document).ready(function(){
+        $("#image").change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $("#show_image").attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+
+</script>
 
 @endsection

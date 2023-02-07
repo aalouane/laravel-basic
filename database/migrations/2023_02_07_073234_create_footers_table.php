2a19be8ed55f5a999860610f1bc38b9e088fe047
Aalouane soufiane <aalouane.s@gmail.com>
2023-02-07 08:45:00 +0100

    footer 2

--- database/migrations/2023_02_07_073234_create_footers_table.php
+++ database/migrations/2023_02_07_073234_create_footers_table.php
@@ -6,26 +6,38 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('footers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('footers', function (Blueprint $table) {
+      $table->id();
+      $table->string('numero')->nullable();
+      $table->string('short_descripton')->nullable();
+      $table->string('country')->nullable();
+      $table->string('adress')->nullable();
+      $table->string('email')->nullable();
+      $table->string('socially_connect')->nullable();
+      $table->string('facebook')->nullable();
+      $table->string('twitter')->nullable();
+      $table->string('behance')->nullable();
+      $table->string('linkedin')->nullable();
+      $table->string('insta')->nullable();
+      $table->string('copyright')->nullable();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('footers');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('footers');
+  }
 };
